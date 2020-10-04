8c55765d7f6134b5d24f81434d5e59f6c62105df
luyongkang <2661089533@qq.com>
2020-10-04 21:16:28 +0800

    ls

---
+++ ls/ls.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<dirent.h>
+
+using namespace std;
+
+int main(int args,char* argv[])
+{
+    DIR* dp;
+    dirent* dirp;
+    if(args!=2)
+        cerr<<"usage:ls directory_name"<<endl;
+
+    if((dp = opendir(argv[1]))==nullptr)
+    {
+        cerr<<"can't open "<<argv[1]<<endl;
+    }
+    else
+    {
+        while((dirp=readdir(dp))!=nullptr)
+        {
+            cout<<dirp->d_name<<endl;
+        }
+    }
+
+    return 0;
+    
+}
