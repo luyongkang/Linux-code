4cb5a32277fb55ce655752e670e58c2e956c3950
luyongkang <2661089533@qq.com>
2020-10-04 23:51:28 +0800

    stdioNo

---
+++ stdIO/io.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<unistd.h>
+
+#define BUFFSIZE 4096
+
+using namespace std;
+int main()
+{
+    int n;
+    char buf[BUFFSIZE];
+
+    while((n=read(STDIN_FILENO,buf,BUFFSIZE))>0)
+    {
+        if(write(STDOUT_FILENO,buf,n)!=n)
+        {
+            cerr<<"write error"<<endl;
+        }
+    }
+
+    if(n<0)
+    {
+        cerr<<"read error"<<endl;
+    }
+}
