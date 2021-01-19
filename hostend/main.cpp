b76fcd708064c777a6def73f82a3bf838dfb7ddd
luyongkang <2661089533@qq.com>
2021-01-19 23:26:44 +0800

    changeStyle

--- hostend/main.cpp
+++ hostend/main.cpp
@@ -5,11 +5,13 @@ using namespace std;
 int main()
 {
 	hostent *phost;
-	while((phost=gethostent())!=nullptr)
+	sethostent(1);
+	while ((phost = gethostent()) != nullptr)
 	{
 		cout << phost->h_name << endl;
 		cout << phost->h_addrtype << endl;
 		cout << "this gethostent" << endl;
+		cout << "-------------------------" << endl;
 	}
 	endhostent();
 	netent *pnet;
@@ -19,6 +21,7 @@ int main()
 		cout << pnet->n_net << endl;
 		cout << pnet->n_addrtype << endl;
 		cout << "this getnetent" << endl;
+		cout << "---------------------------------" << endl;
 	}
 
 	endnetent();
